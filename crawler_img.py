25fd8ae8f0b05e3e517683fdb47e336417fcbe6e
https://github.com/account/ssh <shawn810720@gmail.com>
2017-03-02 19:43:10 +0800

    Update crawler_img.py

--- crawler_img.py
+++ crawler_img.py
@@ -1,6 +1,6 @@
 '''
 |--------------------------------------------------------------------------
-| Google 圖片搜尋爬蟲 - 陽春版
+| 抓 Google 圖片搜尋 - 陽春版
 |--------------------------------------------------------------------------
 | 開發者: Sean@2017/03/01
 |
@@ -31,43 +31,44 @@ import json
 
 
 def createDir(name):
-    dir_name = name + "_" +time.strftime("%Y%m%d_%H_%M_%S")
+    dir_name = name + "_" +time.strftime("%Y%m%d%H%M%S")
 
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
     return dir_name
 
 
-def getImg(keyword='', dirpath='', page=1):
-    print("\n搜尋結果:\n")
-    img_num = 0
+def getImg(keyword='', dirpath=''):
     keyword = urllib.parse.quote(keyword)
+    values = {'searchType' : 'image', 'c2coff': 1, 'safe': 'off', 'hl': 'zh-TW', 'site': 'imghp','tbm': 'isch', 'source': 'hp', 'biw': 1440, 'bih': 803}
+    kwEncode = urllib.parse.urlencode(values)
+    img_num = 0
+    url = 'http://www.google.com/search?'+kwEncode+'&q='+keyword+'&oq='+keyword
+    print('\n搜尋URL:\n'+url)
+    headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'}
+    req = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(req).read()
+    soup = BeautifulSoup(html, "html.parser")
+    print("\n----- 開始抓圖 ----- \n")
 
-    for i in range(page):
+    for item in soup.select(".rg_meta"):
+        img_link = item.text
+        img_link_json = json.loads(img_link)
+        img_num += 1
 
-        url = "http://www.google.com.tw/search?tbm=isch&source=hp&q="+keyword+"&btnG=Search+Images&biw=1920&bih=1075&start="+str(i-1)
-        headers = {'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'}
-        req = urllib.request.Request(url, headers=headers)
-        html = urllib.request.urlopen(req).read()
-        soup = BeautifulSoup(html, "html.parser")
+        try:
+            img_link_json['ity'] = img_link_json['ity'] if not img_link_json['ity']=="" else "jpg"
+            urllib.request.urlretrieve(str(img_link_json['ou']), dirpath +"/"+str(img_num)+'_'+img_link_json['pt']+"."+img_link_json['ity'])
+            print('[ '+str(img_num)+' ] '+img_link_json['ou'])
+            pass
 
-        for item in soup.select(".rg_meta"):
-            img_link = item.text
-            img_link_json = json.loads(img_link)
-            print(img_link_json['ou'])
-            img_num += 1
-
-            try:
-                img_link_json['ity'] = img_link_json['ity'] if not img_link_json['ity']=="" else "jpg"
-                urllib.request.urlretrieve(str(img_link_json['ou']), dirpath +"/"+str(img_num)+"."+img_link_json['ity'])
-                pass
-
-            except:
-                img_num -= 1
-                print("-----save image fail-----")
+        except:
+            img_num -= 1
+            print("----- 圖片存取失敗 -----")
 
         sleep(1)
 
+    print("\n----- 抓圖完成 -----\n")
     print("\n抓到圖片張數: "+str(img_num))
 
 
@@ -77,11 +78,8 @@ def main():
     print("請輸入搜尋關鍵字:")
     keyword = input()
 
-    print("要抓幾頁 (每頁100張) ?")
-    page = int(input())
-
-    dirpath = createDir(keyword)
-    getImg(keyword, dirpath, page)
+    dirpath = createDir('Google 圖片搜尋 - '+keyword)
+    getImg(keyword, dirpath)
 
 
 if __name__ == '__main__':
