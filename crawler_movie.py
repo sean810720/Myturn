da551c921b3e97ad0195c94e4e04bf80f3530534
https://github.com/account/ssh <shawn810720@gmail.com>
2020-01-13 16:21:19 +0800

    Update crawler_movie.py

--- crawler_movie.py
+++ crawler_movie.py
@@ -21,34 +21,47 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 抓開眼電影頁面
+# 抓開眼電影
 res = requests.get("http://www.atmovies.com.tw/movie/now/")
 res.encoding = 'utf8'
 soup = BeautifulSoup(res.text, "html.parser")
 
-print ('\n*** 開眼電影網 - 本週首輪 ***\n')
+print('\n*** 開眼電影網 - 本週首輪 ***\n')
 
 count = 1
 
-for item in soup.select(".filmListAll2 li"):
+for item in soup.select(".filmListPA li"):
 
     # 抓電影基本資料
     movie_title = item.find('a').text
     movie_url = "http://www.atmovies.com.tw"+item.find('a')['href']
-    movie_rating = item.find("font").text
+    movie_id = item.find('a')['href'][7:19]
 
-    # 抓電影簡介
+    # 抓電影明細資料
     res2 = requests.get(movie_url)
     res2.encoding = 'utf8'
-    soup2 = BeautifulSoup(res2.text, "html.parser")
-    intro = soup2.select("#filmTagBlock span")[2]
-    movie_intro = intro.text.strip().lstrip()
+    soup2 = BeautifulSoup(res2.text, "html.parser").select("#filmTagBlock span")
+
+    # 簡介
+    movie_intro = soup2[2].text.strip().lstrip().split()[0]
+
+    # 圖片
+    movie_img = soup2[0].find('img')['src']
+
+    # 抓電影明細資料 - iFrame
+    res3 = requests.get("http://app2.atmovies.com.tw/cfrating/film_ratingdata.cfm?filmid="+movie_id)
+    res3.encoding = 'utf8'
+
+    # IMDB 評分
+    movie_rating = "無評分" if BeautifulSoup(res3.text, "html.parser").find("font") is None else BeautifulSoup(res3.text, "html.parser").find("font").text
 
     # 輸出結果
-    print ('======[',(count),']=========')
-    print ("片名: "+movie_title)
-    print ("網址: "+movie_url)
-    print ("IMDB: "+movie_rating)
-    print ("簡介: \n"+movie_intro)
-    print ("\n\n")
+    print('======[',(count),']=========')
+    print("ID: "+movie_id)
+    print("片名: "+movie_title)
+    print("網址: "+movie_url)
+    print("圖片: "+movie_img)
+    print("IMDB: "+movie_rating)
+    print("簡介: \n"+movie_intro)
+    print("\n\n")
     count += 1
