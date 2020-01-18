07b167e6dfae18692ea2d22903fa739a9b103c01
Sean <sean.chen@starclinic.com.tw>
2020-01-19 00:05:43 +0800

    up

--- crawler_movie.py
+++ crawler_movie.py
@@ -46,66 +46,63 @@ print('\n*** 開眼電影網 - 本週首輪 ***\n')
 count = 1
 
 for item in soup.select(".filmListPA li"):
-    check_new = item.select(".icon-film")
 
-    if len(check_new) == 0:
+    # 抓電影基本資料
+    movie_title = item.find('a').text
+    movie_url = "http://www.atmovies.com.tw"+item.find('a')['href']
+    movie_id = item.find('a')['href'][7:19]
 
-        # 抓電影基本資料
-        movie_title = item.find('a').text
-        movie_url = "http://www.atmovies.com.tw"+item.find('a')['href']
-        movie_id = item.find('a')['href'][7:19]
+    # 抓電影明細資料
+    res2 = requests.get(movie_url)
+    res2.encoding = 'utf8'
+    soup2 = BeautifulSoup(res2.text, "html.parser").select("#filmTagBlock span")
 
-        # 抓電影明細資料
-        res2 = requests.get(movie_url)
-        res2.encoding = 'utf8'
-        soup2 = BeautifulSoup(res2.text, "html.parser").select("#filmTagBlock span")
+    # 簡介
+    movie_intro = soup2[2].text.strip().lstrip().split()[0]
 
-        # 簡介
-        movie_intro = soup2[2].text.strip().lstrip().split()[0]
+    # 圖片
+    movie_img = soup2[0].find('img')['src']
 
-        # 圖片
-        movie_img = soup2[0].find('img')['src']
+    # 片長
+    runtime = soup2[2].select('.runtime li')[0].text[3:].split("分")[0]
 
-        # 片長
-        runtime = soup2[2].select('.runtime li')[0].text[3:].split("分")[0]
+    # 上映日期
+    open_date = soup2[2].select('.runtime li')[1].text[5:]
 
-        # 上映日期
-        open_date = soup2[2].select('.runtime li')[1].text[5:]
+    # 預告片網址
+    youtube_url = BeautifulSoup(res2.text, "html.parser").select(".video_view iframe")[0]["src"]
 
-        # 預告片網址
-        youtube_url = BeautifulSoup(res2.text, "html.parser").select(".video_view iframe")[0]["src"]
+    # 抓電影明細資料 - iFrame
+    res3 = requests.get("http://app2.atmovies.com.tw/cfrating/film_ratingdata.cfm?filmid="+movie_id)
+    res3.encoding = 'utf8'
 
-        # 抓電影明細資料 - iFrame
-        res3 = requests.get("http://app2.atmovies.com.tw/cfrating/film_ratingdata.cfm?filmid="+movie_id)
-        res3.encoding = 'utf8'
+    # IMDB 評分
+    movie_rating = "" if BeautifulSoup(res3.text, "html.parser").find("font") is None else BeautifulSoup(res3.text, "html.parser").find("font").text
 
-        # IMDB 評分
-        movie_rating = "" if BeautifulSoup(res3.text, "html.parser").find("font") is None else BeautifulSoup(res3.text, "html.parser").find("font").text
+    # 輸出結果
+    print('======[',(count),']=========')
+    print("片名: "+movie_title)
+    print("網址: "+movie_url)
+    print("圖片: "+movie_img)
+    print("IMDB: "+movie_rating)
+    print("片長: "+runtime)
+    print("上映日期: "+open_date)
+    print("預告片網址: "+youtube_url)
+    print("簡介: "+movie_intro)
+    print("\n")
 
-        # 輸出結果
-        print('======[',(count),']=========')
-        print("片名: "+movie_title)
-        print("網址: "+movie_url)
-        print("圖片: "+movie_img)
-        print("IMDB: "+movie_rating)
-        print("片長: "+runtime)
-        print("上映日期: "+open_date)
-        print("預告片網址: "+youtube_url)
-        print("簡介: "+movie_intro)
-        print("\n")
+    movie_data.append({
+        "title":movie_title,
+        "url":movie_url,
+        "img_url":movie_img,
+        "imdb_rating":movie_rating,
+        "runtime":runtime,
+        "open_date":open_date,
+        "youtube_url":youtube_url,
+        "movie_intro":movie_intro
+    })
 
-        movie_data.append({
-            "title":movie_title,
-            "url":movie_url,
-            "img_url":movie_img,
-            "imdb_rating":movie_rating,
-            "runtime":runtime,
-            "open_date":open_date,
-            "youtube_url":youtube_url,
-            "movie_intro":movie_intro
-        })
-
-        count += 1
+    count += 1
 
 # 寫入 database reference.
 doc_ref.set(movie_data)
